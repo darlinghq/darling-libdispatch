fbc31200603db68bdc2ca3e44be99f7f2c97dc9e
Lubos Dolezel <lubos@dolezel.info>
2020-02-26 23:19:05 +0100

    Make os/object_private.h compatible with libsystem_trace

--- os/object_private.h
+++ os/object_private.h
@@ -66,7 +66,7 @@
         int volatile ref_cnt; \
         int volatile xref_cnt
 
-#if OS_OBJECT_HAVE_OBJC_SUPPORT
+#if 1 //OS_OBJECT_HAVE_OBJC_SUPPORT
 #define OS_OBJECT_CLASS_SYMBOL(name) OS_##name##_class
 #if TARGET_OS_MAC && !TARGET_OS_SIMULATOR && defined(__i386__)
 #define OS_OBJECT_HAVE_OBJC1 1
